8a474c7f336381f1d7684671600466a465e56245
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-01-11 17:08:29 +0900

    add show article in profile

--- projectapp/models.py
+++ projectapp/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-
 class Project(models.Model):
-    title = models.CharField(max_length=20, null=False)
+    title = models.CharField(max_length=50, null=False)
     description = models.TextField(null=True)
-    image = models.ImageField(upload_to='project/', null=False)
+    image = models.ImageField(upload_to='project/', null=True)
     created_at = models.DateTimeField(auto_now=True)
